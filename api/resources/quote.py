4f081c1f91cdc6014c48e666c566a0b127bb42c5
max <fareolen@gmail.com>
2022-04-11 23:58:52 +0300

    add QuoteListResource

--- api/resources/quote.py
+++ api/resources/quote.py
@@ -3,6 +3,7 @@ from api.models.author import AuthorModel
 from api.models.quote import QuoteModel
 
 
+# /authors/<int:author_id>/quotes/<int:quote_id>
 class QuoteResource(Resource):
     def get(self, author_id=None, quote_id=None):
         """
@@ -11,19 +12,41 @@ class QuoteResource(Resource):
         :param quote_id: id цитаты
         :return: http-response(json, статус)
         """
-        if author_id is None and quote_id is None:  # Если запрос приходит по url: /quotes
-            quotes = QuoteModel.query.all()
-            return [quote.to_dict() for quote in quotes]  # Возвращаем ВСЕ цитаты
-
-        author = AuthorModel.query.get(author_id)
-        if quote_id is None:  # Если запрос приходит по url: /authors/<int:author_id>/quotes
-            quotes = author.quotes.all()
-            return [quote.to_dict() for quote in quotes], 200  # Возвращаем все цитаты автора
-
         quote = QuoteModel.query.get(quote_id)
         if quote:
             return quote.to_dict(), 200
         return {"Error": "Quote not found"}, 404
+
+    def put(self, quote_id):
+        parser = reqparse.RequestParser()
+        parser.add_argument("author")
+        parser.add_argument("text")
+        new_data = parser.parse_args()
+
+        quote = QuoteModel.query.get(quote_id)
+        quote.author = new_data["author"]
+        quote.text = new_data["text"]
+        db.session.commit()
+        return quote.to_dict(), 200
+
+    def delete(self, quote_id):
+        quote = QuoteModel.query.get(quote_id)
+        if quote:
+            quote.delete(), 200
+        return {"Error": "Quote not found"}, 404
+
+
+# /authors/<int:author_id>/quotes
+# /quote
+class QuoteListResource(Resource):
+    def get(self, author_id):
+        if author_id is None:  # Если запрос приходит по url: /quotes
+            quotes = QuoteModel.query.all()
+            return [quote.to_dict() for quote in quotes], 200  # Возвращаем ВСЕ цитаты
+
+        author = AuthorModel.query.get(author_id)
+        quotes = author.quotes.all()
+        return [quote.to_dict() for quote in quotes], 200  # Возвращаем все цитаты автора
 
     def post(self, author_id):
         parser = reqparse.RequestParser()
@@ -39,18 +62,3 @@ class QuoteResource(Resource):
         db.session.add(quote)
         db.session.commit()
         return quote.to_dict(), 201
-
-    def put(self, quote_id):
-        parser = reqparse.RequestParser()
-        parser.add_argument("author")
-        parser.add_argument("text")
-        new_data = parser.parse_args()
-
-        quote = QuoteModel.query.get(quote_id)
-        quote.author = new_data["author"]
-        quote.text = new_data["text"]
-        db.session.commit()
-        return quote.to_dict(), 200
-
-    def delete(self, quote_id):
-        raise NotImplemented("Метод не реализован")
--- app.py
+++ app.py
@@ -1,13 +1,15 @@
 from api import api, app
-from api.resources.quote import QuoteResource
+from api.resources.quote import QuoteResource, QuoteListResource
 from api.resources.author import AuthorResource, AuthorsListResource
 from config import Config
 
 api.add_resource(QuoteResource,
                  '/authors/<int:author_id>/quotes/<int:quote_id>',
+                 )  # <-- requests
+api.add_resource(QuoteListResource,
                  '/authors/<int:author_id>/quotes',
                  '/quotes'
-                 )  # <-- requests
+                 )
 api.add_resource(AuthorResource,
                  '/authors/<int:author_id>')  # <-- requests
 api.add_resource(AuthorsListResource,
